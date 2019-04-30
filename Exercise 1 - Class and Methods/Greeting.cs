a9d3babf8d9b226f8f040c8436e002873dd584c7
Samuel Bittmann <samuel.bittmann@digitecgalaxus.ch>
2019-04-30 18:28:52 +0200

    Added exercise 1: Class and methods

---
+++ Exercise 1 - Class and Methods/Greeting.cs
@@ -0,0 +1,35 @@
+using System;
+
+/*
+Exercise 1
+----------
+
+Modify the code below so that the program returns either a formal or an
+informal greeting according to the following logic:
+If the entered age is below 18 the user should be greeted with
+"Hi, <first name>". If the age is 18 or above, the greeting should read
+"Hello, mr./mrs. <last name>". There needs to be no distinctions between
+genders.
+ */
+public class Greeting
+{
+    public static void Main(string[] args)
+    {
+        int age;
+        if (args.Length <= 0 || !Int32.TryParse(args[2], out age))
+        {
+            Console.WriteLine("Please provide the following arguments: First name, last name and age.");
+            return;
+        }
+        var firstName = args[0];
+        var lastName = args[1];
+
+        var greeting = "";  // (3) Call method using named arguments
+        Console.WriteLine(greeting);
+    }
+
+    public static void GetGreeting()   // (1) Change method signature
+    {
+        // (2) Insert code here
+    }
+}
