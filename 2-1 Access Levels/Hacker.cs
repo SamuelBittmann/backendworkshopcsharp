8286ae0af11f10a8e851455e9ece9edb32349c07
Samuel Bittmann <samuel.bittmann@digitecgalaxus.ch>
2019-10-22 17:04:50 +0200

    Fixed issues with access level exercise

--- 2-1 Access Levels/Hacker.cs
+++ 2-1 Access Levels/Hacker.cs
@@ -4,78 +4,95 @@ using System;
 Exercise 2
 ----------
 
-Oh no, it looks like a hacker has bypassed the security systems of an
-ATM and accesses the account directly. Fix the issues and force the
-attacker to use the intended method "Withdraw(...)" of the ATM class.
+Oh no, it looks like a hacker has bypassed the security systems of an accounting system and uses 
+vulnerabilities in order to steal funds. Fix the issues by setting appropriate access levels and 
+ensure the security features that are present in the BankAccount class cannot be circumvented.
 
-There also seems to be a second problem. An unknowing developer has
-disabled the accessability check because he did not know what it was
-used for. Move the check inside the Account class and prevent
-developers of the ATM class from even seeing the accessability status.
+There also seems to be a second problem. A developer has accidently inserted a bug which leads the
+account holders name to be overwritten in certain cases. Make it impossible to overwrite the inital
+value.
  */
 public class Hacker
 {
     public static void Main(string[] args)
     {
-        decimal amount;
-        if (args.Length < 1 || !decimal.TryParse(args[0], out amount))
-        {
-            Console.WriteLine("Please provide a valid floating point number for the first argument.");
-            return;
-        }
+        var hackersAccount = new BankAccount("Fraudster Black");
+        var victimAccount = new BankAccount("Elsa Fröhlich");
+        hackersAccount.IsEnabled = true;
+        var transferResult = victimAccount
+            .TransferTo(100m, hackersAccount)
+            .Match(
+                () => "Transfer succeeded. The Hacker won.", 
+                () => "Transfer declined. The hacker could not steal money from the victim.");
+        hackersAccount.Balance *= 2;
 
-        var account = new Account();
-        var atm = new ATM(account);
-        account.Withdraw(amount);
-        Console.WriteLine($"New balance: {account.Balance}");
+        Console.WriteLine(transferResult);
+        Console.WriteLine($"The hackers new balance is {hackersAccount.Balance:0.00} (was 100.00)");
     }
 }
 
-public class ATM
+public abstract class Account
 {
-    public Account account;
-
-    public ATM(Account account)
-    {
-        this.account = account;
-    }
-
-    public string Withdraw(decimal amount)
-    {
-        // (3) The accessiblity check seems to have confused a poor developer. Move it inside the Account
-        // class and prevent anyone outside from seeing it.
-        if (account.IsAccessible() || true)
-        {
-            if (account.Balance >= amount)
-            {
-                account.Withdraw(amount);
-            }
-
-            return $"Your new balance is {account.Balance} Fr.";
-        }
-
-        return $"Unable to withdraw {amount} Fr. Your current balance is {account.Balance} Fr.";
-    }
-}
-
-public class Account
-{
-    // (2) It might also be a good idea to hide the balance variable from anyone outside the Account class.
+    // (1) Change the access level of Balance so that it can only be changed by Account or 
+    // BankAccount.
     public decimal Balance;
 
     public Account()
     {
         Balance = 100m;
     }
-    
-    public bool IsAccessible() 
+}
+
+public class BankAccount : Account
+{
+    // (2) Make it impossible to overrwrite the initial value assigned to AccountHolder.
+    public string AccountHolder;
+
+    // (3) Hide also the IsEnabled flag so that it cannot be manipulated from the outside.
+    public bool IsEnabled;
+
+    public BankAccount(string accountHolder)
     {
-        return true;
+        AccountHolder = accountHolder;
+        IsEnabled = !accountHolder.ToLower().StartsWith("fraudster");
     }
 
-    // (1) This method should not be accessible by the hacker.
-    public void Withdraw(decimal amouont)
+    public Result TransferTo(decimal amount, BankAccount destination)
     {
-        Balance -= amouont;
+        if (IsEnabled && destination.IsEnabled && Balance - amount >= 0)
+        {
+            Balance -= amount;
+            destination.Balance += amount;
+            return Result.Success();
+        }
+        else
+        {
+            AccountHolder = "Oups...";
+            return Result.Failure();
+        }
     }
 }
+
+#region Helpers
+// This section contains supporting code that can be ignored for the purposes
+// of this exercise.
+
+public class Result
+{
+    public static Result Success() => new Result(true);
+    public static Result Failure() => new Result(false);
+
+    private bool isSuccess;
+
+    private Result(bool isSuccess)
+    {
+        this.isSuccess = isSuccess;
+    }
+
+    public TOut Match<TOut>(Func<TOut> successMatcher, Func<TOut> failureMatcher)
+    {
+        return isSuccess ? successMatcher() : failureMatcher();
+    }
+}
+
+#endregion
--- 2-2 - Solution/Hacker.cs
+++ 2-2 - Solution/Hacker.cs
@@ -1,82 +1,104 @@
 using System;
-using AtmSystem;
 
 /*
-Exercise 2 (Solution)
+Exercise 2
 ----------
 
-Oh no, it looks like a hacker has bypassed the security systems of an
-ATM and accesses the account directly. Fix the issues and force the
-attacker to use the intended method "Withdraw(...)" of the ATM class.
+Oh no, it looks like a hacker has bypassed the security systems of an accounting system and uses 
+vulnerabilities in order to steal funds. Fix the issues by setting appropriate access levels and 
+ensure the security features that are present in the BankAccount class cannot be circumvented.
 
-There also seems to be a second problem. An unknowing developer has
-disabled the accessability check because he did not know what it was
-used for. Move the check inside the Account class and prevent
-developers of the ATM class from even seeing the accessability status.
+There also seems to be a second problem. A developer has accidently inserted a bug which leads the
+account holders name to be overwritten in certain cases. Make it impossible to overwrite the inital
+value.
  */
 public class Hacker
 {
     public static void Main(string[] args)
     {
-        decimal amount;
-        if (args.Length < 1 || !decimal.TryParse(args[0], out amount))
-        {
-            Console.WriteLine("Please provide a valid floating point number for the first argument.");
-            return;
-        }
+        var hackersAccount = new BankAccount("Fraudster Black");
+        var victimAccount = new BankAccount("Elsa Fröhlich");
+        var transferResult = victimAccount
+            .TransferTo(100m, hackersAccount)
+            .Match(
+                () => "Transfer succeeded. The Hacker won.", 
+                () => "Transfer declined. The hacker could not steal money from the victim.");
 
-        var account = new Account();
-        var atm = new ATM(account);
-        var result = atm.Withdraw(amount);
-        Console.WriteLine(result);
+        Console.WriteLine(transferResult);
+        Console.WriteLine($"The hackers new balance is {hackersAccount.Balance:0.00} (was 100.00)");
     }
 }
 
-namespace AtmSystem
+public abstract class Account
 {
-    public class ATM
+    // The balance is completely protected from outside access and serves as a backing field for the
+    // publicly accessible read only property "Balance".
+    protected decimal balance;
+
+    public decimal Balance => balance;
+
+    // Alternatively you could use an auto property which allows you to set read and write access
+    // levels individually.
+    public decimal BalanceAlternative { get; protected set; }
+
+    public Account()
     {
-        private readonly Account account;
+        balance = 100m;
+    }
+}
 
-        public ATM(Account account)
-        {
-            this.account = account;
-        }
+public class BankAccount : Account
+{
+    // The readonly keyword ensures the value can only be set from within the constructor. Making it
+    // private is not necessary for completing this exercise, but it is always a good idea to 
+    // restrict access as much as possible / reasonable.
+    private readonly string AccountHolder;
 
-        public string Withdraw(decimal amount)
-        {
-            if (account.Balance >= amount)
-            {
-                account.Withdraw(amount);
-                return $"Your new balance is {account.Balance} Fr.";
-            }
-            else
-            {
-                return $"Unable to withdraw {amount} Fr. Your current balance is {account.Balance} Fr.";
-            }
-        }
+    // There is no reason for making the IsEnabled flag accessible from outside, so you should make
+    // it private.
+    private bool IsEnabled;
+
+    public BankAccount(string accountHolder)
+    {
+        AccountHolder = accountHolder;
+        IsEnabled = !accountHolder.ToLower().StartsWith("fraudster");
     }
 
-    public class Account
+    public Result TransferTo(decimal amount, BankAccount destination)
     {
-        public decimal Balance { get; private set; }
-
-        public Account()
+        if (IsEnabled && destination.IsEnabled && balance - amount >= 0)
         {
-            Balance = 100m;
+            balance -= amount;
+            destination.balance += amount;
+            return Result.Success();
         }
-        
-        private bool IsAccessible() 
+        else
         {
-            return true;
-        }
-
-        internal void Withdraw(decimal amouont)
-        {
-            if (IsAccessible())
-            {
-                Balance -= amouont;
-            }
+            return Result.Failure();
         }
     }
 }
+
+#region Helpers
+// This section contains supporting code that can be ignored for the purposes
+// of this exercise.
+
+public class Result
+{
+    public static Result Success() => new Result(true);
+    public static Result Failure() => new Result(false);
+
+    private bool isSuccess;
+
+    private Result(bool isSuccess)
+    {
+        this.isSuccess = isSuccess;
+    }
+
+    public TOut Match<TOut>(Func<TOut> successMatcher, Func<TOut> failureMatcher)
+    {
+        return isSuccess ? successMatcher() : failureMatcher();
+    }
+}
+
+#endregion
